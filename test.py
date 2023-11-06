45afea292e6f2d2d3f81ac1c7de8080de25c390c
BONY MONDAL <bony.mondal@capillarytech.com>
2023-11-06 19:14:08 +0530

    test

--- test.py
+++ test.py
@@ -3,6 +3,17 @@ def string_equality_check_noncompliant(string1, string2):
     if string1 is string2:
         print("The strings are equal.")
 
+def create_db_connection_noncompliant():
+    # Noncompliant: password is hardcoded.
+    connection = psycopg2.connect(
+        database="mydb",
+        user="myuser",
+        password="mypassword",
+        host="localhost",
+        port="5432"
+    )
+    connection.close()
+
 def main():
     string_equality_check_noncompliant("bisd", "bisd")
 
