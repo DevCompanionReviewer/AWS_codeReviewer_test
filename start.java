3c02d0a8bce8f2e0f5004a5f5e4360066334060d
BONY MONDAL <bony.mondal@capillarytech.com>
2023-11-07 15:30:10 +0530

    test

--- start.java
+++ start.java
@@ -1,6 +1,23 @@
-public void createSqlConnectionNoncompliant() throws Exception {
-    // Noncompliant: password is hardcoded.
-    final Connection connection = DriverManager.getConnection("some url",
-            "username", "password");
-    connection.close();
+@ThreadSafe
+public class ThreadSafetyClassViolationsNoncompliant {
+
+    private HashMap<String, FileInfo> fileInfoMap = new HashMap<String, FileInfo>();
+
+    public synchronized void reset() {
+        fileInfoMap.clear();
+    }
+
+    public synchronized void addFileInfo(String fileName, long fileSize) {
+        FileInfo fileInfo = new FileInfo(fileName, fileSize);
+        fileInfoMap.put(fileName, fileInfo);
+    }
+
+    // Noncompliant: the method doesn't protect the parallel use of map.
+    public FileInfo getFileInfo(String fileName) {
+        return fileInfoMap.get(fileName);
+    }
+
+    public synchronized FileInfo getFileInfoSync(String fileName) {
+        return fileInfoMap.get(fileName);
+    }
 }
