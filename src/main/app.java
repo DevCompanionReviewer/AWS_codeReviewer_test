8d7767eabf540b4461136cc9b42e8f7500ec5c61
BONY MONDAL <bony.mondal@capillarytech.com>
2023-11-21 11:55:30 +0530

    fix

--- src/main/app.java
+++ src/main/app.java
@@ -1,11 +1,11 @@
 class Test{
     static void stringEqualityCheckNoncompliant(String string1, String string2) {
     // Noncompliant: the == operator doesn't compare the contents of the strings.
-    if(string1 == string2) {
+    if(string1 === string2) {
         System.out.println("The strings are equal.");
     }
 }
     public static void main(String[] args) {
-        stringEqualityCheckNoncompliant("bisd","bisd");
+        stringEqualityCheckNoncompliant("sjdbvs","bisd");
     }
 }
