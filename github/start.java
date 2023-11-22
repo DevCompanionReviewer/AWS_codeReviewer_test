689322a1f70dc7b9acd2cfc75f34db6bc182eca5
Nikhil-Capillary <nikhil.sharma@capillarytech.com>
2023-11-22 18:12:18 +0530

    test

--- github/start.java
+++ github/start.java
@@ -1,7 +1,7 @@
 class Test{
     static void stringEqualityCheckNoncompliant(String string1, String string2) {
     // Noncompliant: the == operator doesn't compare the contents of the strings.
-    if(string1 == string2) {
+    if(string1 === string2) {
         System.out.println("The strings are equal.");
     }
 }
