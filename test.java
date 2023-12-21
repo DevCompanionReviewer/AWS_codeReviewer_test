b508b6c1459f4be2251c6d8726a427d9a023b74e
BONY MONDAL <bony.mondal@capillarytech.com>
2023-12-21 18:44:51 +0530

    test

--- test.java
+++ test.java
@@ -4,15 +4,33 @@ public class SecureJavaCode {
     public static void main(String[] args) {
         // Use Scanner to get user input
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your name: ");
+        try {
+            System.out.print("Enter your name: ");
 
-        // Read user input
-        String name = scanner.nextLine();
+            // Read user input and validate for potential command injection
+            String name = scanner.nextLine();
+            if (!isValidInput(name)) {
+                System.out.println("Invalid input. Please enter a valid name.");
+                return;
+            }
 
-        // Print a welcome message
-        System.out.println("Hello, " + name + "! Welcome to secure Java coding.");
+            // Print a welcome message
+            System.out.println("Hello, " + name + "! Welcome to secure Java coding.");
+        } catch (Exception e) {
+            // Handle exceptions, log or report as necessary
+            System.err.println("An error occurred: " + e.getMessage());
+        } finally {
+            // Close the scanner to avoid resource leaks
+            if (scanner != null) {
+                scanner.close();
+            }
+        }
+    }
 
-        // Close the scanner to avoid resource leaks
-        scanner.close();
+    // Validate input to prevent command injection
+    private static boolean isValidInput(String input) {
+        // Implement your validation logic, e.g., using regular expressions
+        // This is a simple example; adjust based on your specific requirements
+        return input.matches("[a-zA-Z]+");
     }
 }
