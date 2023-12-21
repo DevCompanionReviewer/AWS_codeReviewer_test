2e8e47bf385b4fdb41f6c4aac47182a75827900e
BONY MONDAL <bony.mondal@capillarytech.com>
2023-12-21 14:12:37 +0530

    test

--- test.java
+++ test.java
@@ -1,11 +1,19 @@
-class Test{
-    static void stringEqualityCheckNoncompliant(String string1, String string2) {
-    // Noncompliant: the == operator doesn't compare the contents of the strings.
-    if(string1.equals(string2)) {
-        System.out.println("The strings are equal.");
+public class Factorial {
+
+    // Recursive method to calculate factorial
+    public static int calculateFactorial(int n) {
+        // Base case
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            // Recursive case
+            return n * calculateFactorial(n - 1);
+        }
     }
-}
+
     public static void main(String[] args) {
-        stringEqualityCheckNoncompliant("bisd","bisd");
+        // Calculate and display the factorial of 5
+        int result = calculateFactorial(5);
+        System.out.println("Factorial of 5 is: " + result);
     }
 }
