94bdeea26f6888f1675818a7e2f0ea03f98e9ebd
BONY MONDAL <bony.mondal@capillarytech.com>
2023-12-21 18:46:29 +0530

    test

--- test.java
+++ test.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class SecureJavaCode {
+    private static final Logger LOGGER = Logger.getLogger(SecureJavaCode.class.getName());
+
     public static void main(String[] args) {
         // Use Scanner to get user input
         Scanner scanner = new Scanner(System.in);
@@ -17,12 +20,17 @@ public class SecureJavaCode {
             // Print a welcome message
             System.out.println("Hello, " + name + "! Welcome to secure Java coding.");
         } catch (Exception e) {
-            // Handle exceptions, log or report as necessary
-            System.err.println("An error occurred: " + e.getMessage());
+            // Handle exceptions, log a generic error message, and log the detailed error information securely
+            System.err.println("An error occurred. Please contact the administrator for assistance.");
+            LOGGER.severe("Exception occurred: " + e.getMessage());
         } finally {
             // Close the scanner to avoid resource leaks
             if (scanner != null) {
-                scanner.close();
+                if (closeScanner(scanner)) {
+                    System.out.println("Scanner closed successfully.");
+                } else {
+                    System.err.println("Error closing the scanner.");
+                }
             }
         }
     }
@@ -32,5 +40,16 @@ public class SecureJavaCode {
         // Implement your validation logic, e.g., using regular expressions
         // This is a simple example; adjust based on your specific requirements
         return input.matches("[a-zA-Z]+");
+    }
+
+    // Close the scanner and check for errors
+    private static boolean closeScanner(Scanner scanner) {
+        try {
+            scanner.close();
+            return true;
+        } catch (Exception e) {
+            LOGGER.warning("Error closing scanner: " + e.getMessage());
+            return false;
+        }
     }
 }
