84741e1f97eb28cfea29a41bc17ffb912a0dd4aa
BONY MONDAL <bony.mondal@capillarytech.com>
2023-12-21 14:25:41 +0530

    test

--- test.java
+++ test.java
@@ -1,19 +1,28 @@
-public class Factorial {
-
-    // Recursive method to calculate factorial
-    public static int calculateFactorial(int n) {
-        // Base case
-        if (n == 0 || n == 1) {
-            return 1;
-        } else {
-            // Recursive case
-            return n * calculateFactorial(n - 1);
-        }
-    }
+public class SecureCodeExample {
 
     public static void main(String[] args) {
-        // Calculate and display the factorial of 5
-        int result = calculateFactorial(5);
-        System.out.println("Factorial of 5 is: " + result);
+        // Declare and initialize variables securely
+        String username = "user123";
+        char[] password = "securePassword123".toCharArray();
+
+        // Use a secure method for password hashing
+        String hashedPassword = hashPassword(password);
+
+        // Print the hashed password
+        System.out.println("Hashed Password: " + hashedPassword);
+    }
+
+    // A secure method for password hashing
+    private static String hashPassword(char[] password) {
+        // Use a strong hashing algorithm, such as BCrypt
+        // In a real-world scenario, use a dedicated library for password hashing
+        // BCrypt is just an example; in practice, use a proven library like bcrypt or Argon2
+        String hashedPassword = BCrypt.hashpw(new String(password), BCrypt.gensalt());
+
+        // Clear the original password from memory after hashing
+        // This helps minimize the risk of exposing the password
+        Arrays.fill(password, '\0');
+
+        return hashedPassword;
     }
 }
