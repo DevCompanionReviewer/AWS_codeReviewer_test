8400970da9425a0681e7526ea276e064b23cbc2d
BONY MONDAL <bony.mondal@capillarytech.com>
2023-12-22 17:27:35 +0530

    comment if changes required

--- test.java
+++ test.java
@@ -5,6 +5,54 @@ public class SecureJavaCode {
     private static final Logger LOGGER = Logger.getLogger(SecureJavaCode.class.getName());
 
     public static void main(String[] args) {
+        // Module 1: Hello World
+        printHelloWorld();
+
+        // Module 2: Basic Arithmetic
+        int result = addNumbers(5, 10);
+        System.out.println("Result of addition: " + result);
+
+        // Module 3: Looping
+        printNumbersInRange(1, 5);
+
+        // Module 4: Arrays
+        int[] array = {1, 2, 3, 4, 5};
+        printArrayElements(array);
+
+        // ... Continue adding more modules as needed
+
+    }
+
+    // Module 1
+    private static void printHelloWorld() {
+        System.out.println("Hello, World!");
+    }
+
+    // Module 2
+    private static int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    // Module 3
+    private static void printNumbersInRange(int start, int end) {
+        for (int i = start; i <= end; i++) {
+            System.out.println(i);
+        }
+    }
+
+    // Module 4
+    private static void printArrayElements(int[] array) {
+        for (int element : array) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+    }
+
+    // ... Define more modules as necessary
+
+
+
+    public static void main(String[] args) {
         // Use Scanner to get user input
         Scanner scanner = new Scanner(System.in);
         try {
@@ -53,5 +101,121 @@ public class SecureJavaCode {
         } catch (Exception e) {
             LOGGER.warning("Error closing scanner: " + e.getMessage());
         }
+    }
+}
+enum TransactionType {
+    DEPOSIT, WITHDRAWAL, TRANSFER
+}
+
+class Transaction {
+    private TransactionType type;
+    private double amount;
+
+    public Transaction(TransactionType type, double amount) {
+        this.type = type;
+        this.amount = amount;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+}
+
+class Account {
+    private String accountNumber;
+    private double balance;
+    private List<Transaction> transactions;
+
+    public Account(String accountNumber, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+        this.transactions = new ArrayList<>();
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+        transactions.add(new Transaction(TransactionType.DEPOSIT, amount));
+    }
+
+    public void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            transactions.add(new Transaction(TransactionType.WITHDRAWAL, amount));
+        } else {
+            System.out.println("Insufficient funds!");
+        }
+    }
+
+    public void transfer(Account recipient, double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            recipient.deposit(amount);
+            transactions.add(new Transaction(TransactionType.TRANSFER, amount));
+        } else {
+            System.out.println("Insufficient funds for transfer!");
+        }
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+}
+
+class Bank {
+    private List<Account> accounts;
+
+    public Bank() {
+        this.accounts = new ArrayList<>();
+    }
+
+    public void addAccount(Account account) {
+        accounts.add(account);
+    }
+
+    public Account getAccount(String accountNumber) {
+        for (Account account : accounts) {
+            if (account.getAccountNumber().equals(accountNumber)) {
+                return account;
+            }
+        }
+        return null;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Bank bank = new Bank();
+
+        // Create accounts
+        Account account1 = new Account("A12345", 1000);
+        Account account2 = new Account("B67890", 500);
+
+        // Add accounts to the bank
+        bank.addAccount(account1);
+        bank.addAccount(account2);
+
+        // Perform transactions
+        account1.deposit(500);
+        account1.withdraw(200);
+        account1.transfer(account2, 300);
+
+        // Print account balances and transactions
+        System.out.println("Account 1 Balance: $" + account1.getBalance());
+        System.out.println("Account 2 Balance: $" + account2.getBalance());
+
+        System.out.println("Account 1 Transactions: " + account1.getTransactions());
+        System.out.println("Account 2 Transactions: " + account2.getTransactions());
     }
 }
