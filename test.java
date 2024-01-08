5d3e1eeeed656e536a25a2d65670549ee53ce99f
BONY MONDAL <bony.mondal@capillarytech.com>
2024-01-08 14:07:03 +0530

    test

--- test.java
+++ test.java
@@ -1,13 +1,30 @@
-import java.util.Scanner;
+import java.util.Objects;
 
-public class SecureInputExample {
+public class StringEqualityExample {
+    public static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "World";
+        String str3 = "Hello";
+        String str4 = null;
 
-   public void executeSqlStatementNoncompliant(HttpServletRequest request, java.sql.Connection connection) {
-    final String favoriteColor = request.getParameter("favoriteColor");
-    try {
-        // Noncompliant: user-given input is not sanitized before use.
-    } catch (java.sql.SQLException e) {
-        throw new RuntimeException(e);
+        // Using Objects.equals() for safe string equality comparison
+        if (Objects.equals(str1, str2)) {
+            System.out.println("str1 and str2 are equal");
+        } else {
+            System.out.println("str1 and str2 are not equal");
+        }
+
+        if (Objects.equals(str1, str3)) {
+            System.out.println("str1 and str3 are equal");
+        } else {
+            System.out.println("str1 and str3 are not equal");
+        }
+
+        // Handling null values
+        if (Objects.equals(str1, str4)) {
+            System.out.println("str1 and str4 are equal");
+        } else {
+            System.out.println("str1 and str4 are not equal");
+        }
     }
-}
 }
