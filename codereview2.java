b6a1dd96a712bbf49141a4db654f4a621dc93771
Pallav <pallav.kumar@capillarytech.com>
2024-02-09 21:54:38 +0530

    Create codereview2.java

---
+++ codereview2.java
@@ -0,0 +1,36 @@
+public class RuntimeIssuesDemo {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3};
+        System.out.println(numbers[3]); 
+
+        
+        String str = null;
+        System.out.println(str.length()); 
+
+        
+        int result = 10 / 0; 
+
+    
+        String numStr = "abc";
+        int num = Integer.parseInt(numStr);
+
+        
+        Object obj = new Integer(10);
+        String strObj = (String) obj;
+
+        
+        List<String> names = new ArrayList<>();
+        names.add("Alice");
+        names.add("Bob");
+        for (String name : names) {
+            names.remove(name);
+        }
+
+        
+        List<Integer> bigList = new ArrayList<>();
+        while (true) {
+            bigList.add(999999999);
+        }
+    }
+}
